4edfea8fc2b1235cf08e65eab2373f37fde6f26d
Jose Albizures <jose@albizures.com>
2021-01-05 15:31:44 +0300

    add success message

--- src/components/Submission.tsx
+++ src/components/Submission.tsx
@@ -4,7 +4,75 @@ import 'firebase/database';
 import 'firebase/auth';
 import { Input, Label } from './FormFields';
 
+interface SuccessMessageProps {
+	onClose: () => void;
+}
+
+const SuccessMessage: React.FC<SuccessMessageProps> = (props) => {
+	const [isSuccess, setSuccessStatus] = React.useState(true);
+	const { onClose } = props;
+	const timeoutRef = React.useRef(null);
+
+	React.useEffect(() => {
+		if (!isSuccess) {
+			return;
+		}
+
+		clearTimeout(timeoutRef.current);
+
+		timeoutRef.current = setTimeout(() => {
+			setSuccessStatus(false);
+			setTimeout(() => {
+				onClose();
+			}, 600);
+		}, 1000 * 4);
+
+		return () => clearTimeout(timeoutRef.current);
+	}, [isSuccess, onClose]);
+
+	return (
+		<div
+			className={`fixed success ${
+				isSuccess ? 'fadeInUp' : 'fadeOutDown'
+			} shadow overflow-hidden rounded items-center bg-white border mt-4 mx-auto flex`}
+		>
+			<div className="bg-green-500 p-2">
+				<svg
+					fill="none"
+					viewBox="0 0 24 24"
+					className="w-8 h-8 inline-block text-white"
+					stroke="currentColor"
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						strokeWidth="2"
+						d="M5 13l4 4L19 7"
+					/>
+				</svg>
+			</div>
+			<p className="pl-4">The link has been submitted</p>
+			<button onClick={onClose} className="flex justify-end flex-1 m-2 p-2">
+				<svg
+					fill="none"
+					viewBox="0 0 24 24"
+					className="w-4 h-4 text-gray-600"
+					stroke="currentColor"
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						strokeWidth="2"
+						d="M6 18L18 6M6 6l12 12"
+					/>
+				</svg>
+			</button>
+		</div>
+	);
+};
+
 export const Submission = () => {
+	const [isSuccess, setSuccessStatus] = React.useState(false);
 	const refProposal = React.useRef<HTMLInputElement>();
 	React.useEffect(() => {
 		if (firebase.apps.length > 0) {
@@ -22,9 +90,12 @@ export const Submission = () => {
 		});
 	}, []);
 
+	const onCloseSuccess = () => {
+		setSuccessStatus(false);
+	};
+
 	const onSubmit = async (event: React.FormEvent) => {
 		event.preventDefault();
-
 		const result = await new firebase.auth.RecaptchaVerifier(
 			'proposal-submit',
 			{
@@ -35,31 +106,33 @@ export const Submission = () => {
 		const ref = await firebase.database().ref('proposals').push().set({
 			link: refProposal.current.value,
 			added: false,
+			date: new Date(),
 		});
 
 		refProposal.current.value = '';
+		setSuccessStatus(true);
 	};
 	return (
-		<form
-			className="py-6 px-2 md:px-6 mt-4 rounded bg-gray-200 border border-gray-300"
-			onSubmit={onSubmit}
-		>
-			<h3 className="text-2xl mb-3 text-center">
-				Do you have something to share?
-			</h3>
-			<Label htmlFor="link">Write the link here</Label>
-			<Input
-				ref={refProposal}
-				name="link-proposal"
-				id="link-proposal"
-				placeholder="http://example.com"
-			/>
-			<div id="proposal-submit"></div>
-			<div className="w-full flex items-center justify-center mt-5">
-				<button className="rounded border border-blue-700 bg-blue-700 text-white px-4 py-2">
-					Submit
-				</button>
-			</div>
-		</form>
+		<div className="py-6 px-2 md:px-6 mt-4 rounded bg-gray-200 border border-gray-300">
+			<form onSubmit={onSubmit}>
+				<h3 className="text-2xl mb-3 text-center">
+					Do you have something to share?
+				</h3>
+				<Label htmlFor="link">Write the link here</Label>
+				<Input
+					ref={refProposal}
+					name="link-proposal"
+					id="link-proposal"
+					placeholder="http://example.com"
+				/>
+				<div id="proposal-submit"></div>
+				<div className="w-full flex items-center justify-center mt-5">
+					<button className="rounded border border-blue-700 bg-blue-700 text-white px-4 py-2">
+						Submit
+					</button>
+				</div>
+			</form>
+			{isSuccess && <SuccessMessage onClose={onCloseSuccess} />}
+		</div>
 	);
 };
