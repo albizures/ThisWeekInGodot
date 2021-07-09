fd9114e269021e34e9d6c8b2b70edc85c94bf53b
Jose Albizures <jose@albizures.com>
2021-07-09 11:29:35 +0300

    add styles and use absolute url in the feed xml

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import inlineCSS from 'inline-css';
 import RSS from 'rss';
 import Link from 'next/link';
 import { GetStaticProps } from 'next';
@@ -14,39 +15,55 @@ const rss = new RSS({
 	site_url: config.base_url,
 });
 
-export const getStaticProps: GetStaticProps<{ props?: unknown }> = async () => {
-	const posts = listPostContent(1, config.posts_per_page);
+export const getStaticProps: GetStaticProps<{ props?: unknown }> =
+	async () => {
+		const posts = listPostContent(1, config.posts_per_page);
 
-	posts
-		.filter((post) => {
-			if (!post.tags) {
-				return true;
-			}
+		await Promise.all(
+			posts
+				.filter((post) => {
+					if (!post.tags) {
+						return true;
+					}
 
-			return !post.tags.includes('draft');
-		})
-		.forEach((post) => {
-			rss.item({
-				date: post.date,
-				title: post.title,
-				description: compile(post.content, Layouts.post, post),
-				url: `${config.base_url}/posts/${post.slug}`,
-			});
-		});
+					return !post.tags.includes('draft');
+				})
+				.map((post) => {
+					return inlineCSS(
+						compile(post.content, Layouts.post, post),
+						{
+							url: `file://${process.cwd()}/`,
+							applyLinkTags: true,
+							removeHtmlSelectors: true,
+						},
+					)
+						.then(absolutePaths)
+						.then((html: string) => {
+							rss.item({
+								date: post.date,
+								title: post.title,
+								description: absolutePaths(html),
+								url: `${config.base_url}/posts/${post.slug}`,
+							});
+						});
+				}),
+		);
 
-	fs.writeFileSync('./public/rss.xml', rss.xml());
+		fs.writeFileSync('./public/rss.xml', rss.xml());
 
-	return {
-		props: {},
+		return {
+			props: {},
+		};
 	};
-};
 
 const Rss = () => (
 	<Layout>
 		<h1 className="text-3xl text-center">
 			You're probably looking for this{' '}
 			<Link href="/rss.xml">
-				<a className="underline text-blue-700 hover:text-blue-600">/rss.xml</a>
+				<a className="underline text-blue-700 hover:text-blue-600">
+					/rss.xml
+				</a>
 			</Link>
 		</h1>
 	</Layout>
