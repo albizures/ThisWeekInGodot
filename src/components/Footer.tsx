3e77526ffcaf9c536809a0e801ac453c06ceae6c
Jose Albizures <albizures3601@gmail.com>
2022-01-09 19:07:39 +0300

    create issue #67

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,10 +8,14 @@ export const Footer = () => {
 		<footer>
 			<div>
 				<div className="flex justify-center space-x-4 flex-wrap">
-					<ExternalLink href={`https://twitter.com/${config.twitter_account}`}>
+					<ExternalLink
+						href={`https://twitter.com/${config.twitter_account}`}
+					>
 						Twitter
 					</ExternalLink>
-					<ExternalLink href={`https://github.com/${config.github_account}`}>
+					<ExternalLink
+						href={`https://github.com/${config.github_account}`}
+					>
 						GitHub
 					</ExternalLink>
 					<Link href="/rss.xml">
@@ -25,7 +29,7 @@ export const Footer = () => {
 					</Link>
 				</div>
 				<p className="text-center my-2">
-					© 2020 by Jose Albizures. All rights reserved.
+					© 2022 by Jose Albizures. All rights reserved.
 				</p>
 			</div>
 			<div className="text-center mb-4">Made with 💙</div>
