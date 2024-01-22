f1232e29a18de43be40a4cfbd56cf7dfde64d72d
Jose Albizures <jose@albizures.com>
2024-01-21 18:41:30 -0600

    feat: update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,23 @@ import Link from 'next/link';
 export const Footer = () => {
 	return (
 		<footer>
-			<div>
+			<div className="bg-gray-400">
+				<div className="container max-w-screen-md text-center py-4">
+					<h3 className="text-xl font-bold">Other projects</h3>
+					<div className="flex gap-4 justify-center mt-2">
+						<a
+							className="underline"
+							href="https://flowmodoro.vyke.dev/"
+						>
+							Flowmodoro app
+						</a>
+						<a className="underline" href="https://clock.vyke.dev/">
+							Online clock
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className="bg-gray-400 py-2 border-t border-gray-500">
 				<div className="flex justify-center space-x-4 flex-wrap">
 					<ExternalLink
 						href={`https://twitter.com/${config.twitter_account}`}
@@ -28,11 +44,13 @@ export const Footer = () => {
 						<a className="underline">Privacy Policy</a>
 					</Link>
 				</div>
-				<p className="text-center my-2">
+				<p className="text-center py-2">
 					© 2022 by Jose Albizures. All rights reserved.
 				</p>
 			</div>
-			<div className="text-center mb-4">Made with 💙</div>
+			<div className="text-center pb-4 bg-gray-400">
+				<span>Made with 💙</span>
+			</div>
 		</footer>
 	);
 };
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,58 +11,60 @@ type Props = {
 
 export default function Layout({ children }: Props) {
 	return (
-		<div className="root container px-2 max-w-screen-md">
-			<Head>
-				<meta charSet="utf-8" />
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1"
-				/>
-				<link rel="manifest" href="/site.webmanifest" />
-				<link
-					href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Roboto:ital,wght@0,300;1,300&display=swap"
-					rel="stylesheet"
-				></link>
-				<link rel="apple-touch-icon" href="/favicon.png" />
-				<meta name="theme-color" content="#fff" />
-			</Head>
-			<nav className="mt-4">
-				<h1 className="text-4xl text-center font-heading">
-					<Link href="/">
-						<a>This Week in Godot</a>
-					</Link>
-				</h1>
-				<div className="flex">
-					<ul className="flex justify-center space-x-4 w-full">
-						<li>
-							<ExternalLink
-								href={`https://twitter.com/${config.twitter_account}`}
-							>
-								Twitter
-							</ExternalLink>
-						</li>
-						<li>
-							<ExternalLink
-								href={`https://github.com/${config.github_account}`}
-							>
-								GitHub
-							</ExternalLink>
-						</li>
-						<li>
-							<Link href="/rss.xml">
-								<a className="underline">RSS</a>
-							</Link>
-						</li>
-					</ul>
-				</div>
-			</nav>
-			<main className="mb-4">{children}</main>
+		<>
+			<div className="root container px-2 max-w-screen-md">
+				<Head>
+					<meta charSet="utf-8" />
+					<meta
+						name="viewport"
+						content="width=device-width, initial-scale=1"
+					/>
+					<link rel="manifest" href="/site.webmanifest" />
+					<link
+						href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Roboto:ital,wght@0,300;1,300&display=swap"
+						rel="stylesheet"
+					></link>
+					<link rel="apple-touch-icon" href="/favicon.png" />
+					<meta name="theme-color" content="#fff" />
+				</Head>
+				<nav className="mt-4">
+					<h1 className="text-4xl text-center font-heading">
+						<Link href="/">
+							<a>This Week in Godot</a>
+						</Link>
+					</h1>
+					<div className="flex">
+						<ul className="flex justify-center space-x-4 w-full">
+							<li>
+								<ExternalLink
+									href={`https://twitter.com/${config.twitter_account}`}
+								>
+									Twitter
+								</ExternalLink>
+							</li>
+							<li>
+								<ExternalLink
+									href={`https://github.com/${config.github_account}`}
+								>
+									GitHub
+								</ExternalLink>
+							</li>
+							<li>
+								<Link href="/rss.xml">
+									<a className="underline">RSS</a>
+								</Link>
+							</li>
+						</ul>
+					</div>
+				</nav>
+				<main className="mb-4">{children}</main>
+			</div>
 			<Footer />
 			<script
 				data-goatcounter="https://thisweekingodot.goatcounter.com/count"
 				async
 				src="//gc.zgo.at/count.js"
 			></script>
-		</div>
+		</>
 	);
 }
